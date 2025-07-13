aa95d593df401427db0b86ea5b1afcf7f43c2a40
Amitha Vikraman <amithavikraman@Amithas-MacBook-Air.local>
2025-07-13 15:15:46 +0530

    Initial commit for caption-service

---
+++ caption-service/src/main/java/com/example/caption/caption_service/CaptionServiceApplication.java
@@ -0,0 +1,13 @@
+package com.example.caption.caption_service;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CaptionServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CaptionServiceApplication.class, args);
+	}
+
+}
---
+++ caption-service/src/test/java/com/example/caption/caption_service/CaptionServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.caption.caption_service;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CaptionServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
