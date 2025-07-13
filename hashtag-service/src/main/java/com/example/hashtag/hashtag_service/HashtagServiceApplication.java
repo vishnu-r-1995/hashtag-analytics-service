4bb206202ddf89339d8772656d66d077dac532fa
Vishnu R <vishnuvjd@gmail.com>
2025-07-13 17:54:37 +0530

    Initial commit for hashtag-service

---
+++ hashtag-service/src/main/java/com/example/hashtag/hashtag_service/HashtagServiceApplication.java
@@ -0,0 +1,13 @@
+package com.example.hashtag.hashtag_service;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HashtagServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HashtagServiceApplication.class, args);
+	}
+
+}
---
+++ hashtag-service/src/test/java/com/example/hashtag/hashtag_service/HashtagServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.hashtag.hashtag_service;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HashtagServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
